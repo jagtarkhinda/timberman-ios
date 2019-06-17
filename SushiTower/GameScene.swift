5d5ee4884d656a9cd5da972df9e6361b35dad84c
MacStudent <macstudent@130-IMAC-24.local>
2019-06-17 11:25:55 -0400

    adding class code

--- SushiTower/GameScene.swift
+++ SushiTower/GameScene.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Parrot. All rights reserved.
 //
 
+
 import SpriteKit
 import GameplayKit
 
@@ -14,8 +15,10 @@ class GameScene: SKScene {
     let cat = SKSpriteNode(imageNamed: "character1")
     let sushiBase = SKSpriteNode(imageNamed:"roll")
     
+    var tower:[SKSpriteNode]  = []
     
-
+    
+    
     
     override func didMove(to view: SKView) {
         // add background
@@ -33,21 +36,64 @@ class GameScene: SKScene {
         sushiBase.position = CGPoint(x:self.size.width*0.5, y: 100)
         addChild(sushiBase)
         
-        let specialSushi = SKSpriteNode(imageNamed: "roll")
-         specialSushi.position = CGPoint(x:self.size.width*0.5, y: 500)
-        addChild(specialSushi)
         
-        let chop = SKSpriteNode(imageNamed: "chopstick")
-        chop.position.x = -100
-        //adding child to another object (now the position will be relative to the parent object
-        specialSushi.addChild(chop)
+        for i in 0...5 {
+            spawnSushi()
+        }
         
         
     }
     
     
+    func spawnSushi() {
+        
+        // 1. check how many pieces are in teh tower
+        let numPieces = self.tower.count
+        
+        // if 0 pieces, put piece at the starting position
+        let specialSushi = SKSpriteNode(imageNamed:"roll")
+        
+        if (numPieces  == 0) {
+            // add sushi at starting position
+            specialSushi.position = CGPoint(x:self.size.width*0.5, y: 200)
+        }
+        else if (numPieces > 0) {
+            // 1. get the previous piece
+            let previousPiece = self.tower[self.tower.count-1]
+            
+            // 2. add the new pieces at 50 pixel above it
+            specialSushi.position = CGPoint(
+                x:self.size.width*0.5,
+                y:previousPiece.position.y + 100)
+        }
+        
+        
+        let rightStick = SKSpriteNode(imageNamed: "chopstick")
+        rightStick.position.x = 80;
+        rightStick.xScale = -1
+        
+        let leftStick = SKSpriteNode(imageNamed:"chopstick")
+        leftStick.position.x = -80
+        
+        // add sticks to sushi
+        specialSushi.addChild(leftStick)
+        specialSushi.addChild(rightStick)
+        
+        // add sushi to the screen
+        addChild(specialSushi)
+        
+        // add the sushi to the tower
+        tower.append(specialSushi)
+        
+    }
+    
+    
+    
     
     override func update(_ currentTime: TimeInterval) {
+        //specialSushi.position.y = specialSushi.position.y - 10
+        //chopstick.position.y = chopstick.position.y - 10
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -60,27 +106,24 @@ class GameScene: SKScene {
         
         print(mousePosition)
         
-        
-        let CENTER = self.size.width/2
-        
-        if(mousePosition.x < CENTER)
-        {
-            //if right side
-            //write the code to position the cat at 80%
+        let CENTER = self.size.width / 2
+        if (mousePosition.x < CENTER) {
+            // left
+            // - move cat to x = 25% of width
             cat.position = CGPoint(x:self.size.width*0.25, y:100)
-           // flipping cat
-            let leftHand = SKAction.scaleX(to: 1, duration: 0)
-            self.cat.run(leftHand)
-        }
-        else{
             
-            //if left side
-            //write the code to position the cat at 25%
-             cat.position = CGPoint(x:self.size.width*0.80, y:100)
-            //flipping cat
-            let leftHand = SKAction.scaleX(to: -1, duration: 0)
-            self.cat.run(leftHand)
+            let leftHandUpAction = SKAction.scaleX(to: 1, duration: 0)
+            self.cat.run(leftHandUpAction)
         }
+        else {
+            // right
+            // - move cat to x = 80% of width
+            cat.position = CGPoint(x:self.size.width*0.80, y:100)
+            let rightHandUpAction = SKAction.scaleX(to: -1, duration: 0)
+            self.cat.run(rightHandUpAction)
+        }
+        
+        
     }
     
 }
